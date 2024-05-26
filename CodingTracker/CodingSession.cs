7c2b62a89aa1093bcbba40c959f09314862896fb
Seth Dossett <95620779+SethDossett@users.noreply.github.com>
2024-05-26 11:50:04 -0400

    checks start and end time

--- CodingTracker/CodingSession.cs
+++ CodingTracker/CodingSession.cs
@@ -18,15 +18,39 @@
         public static void StartTimer()
         {
             Console.WriteLine("\n\nCoding Session Has Begun.\n\n");
-            DateTime now = DateTime.Now;
-            if (DateTime.TryParse(now.ToString(), out now))
+
+
+
+            DateTime startTime = DateTime.Now;
+            DateTime endTime;
+            if (DateTime.TryParse(startTime.ToString(), out startTime))
             {
-                Console.WriteLine(now.ToString("MM-dd-yy HH:mm:ss"));
+                Console.WriteLine(startTime.ToString("MM-dd-yy HH:mm:ss"));
             }
             else
             {
                 Console.Error.WriteLine("DateTime UnParsable.");
             }
+
+            Console.WriteLine("Press 0 to End Coding Session.");
+            string answer = Console.ReadLine();
+
+            //Check to End
+            while (answer != "0") answer = Console.ReadLine();
+
+
+            endTime = DateTime.Now;
+
+            if (DateTime.TryParse(endTime.ToString(), out endTime))
+            {
+                Console.WriteLine(endTime.ToString("MM-dd-yy HH:mm:ss"));
+            }
+            else
+            {
+                Console.Error.WriteLine("DateTime UnParsable.");
+            }
+
+
         }
         public static void EndTimer()
         {
